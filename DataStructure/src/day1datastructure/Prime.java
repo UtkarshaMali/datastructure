2265f743ea27899e3e5164683bf6774186f3b66c
Utkarsha <utkarshamali111@gmail.com>
2023-06-27 10:39:40 +0530

    My data-structure

---
+++ DataStructure/src/day1datastructure/Prime.java
@@ -0,0 +1,5 @@
+package day1datastructure;
+
+public class Prime {
+
+}
---
+++ DataStructure/src/day1datastructure/StarPattern.java
@@ -0,0 +1,21 @@
+package day1datastructure;
+
+public class StarPattern {
+    public static void LeftSideSymboltriangle(int k) {
+    	int a,b;
+    	for (a = 0; a<k; a++) {
+    		for(b=2*(k-a); b>=0; b-- ) {
+    			System.out.print(" ");
+    		}
+    			for(b=0; b<=a; b++) {
+    				System.out.print("* ");
+    			}
+    			System.out.println();
+    		}
+    	}
+    	public static void main(String args[]) {
+    		int k= 4;
+    		LeftSideSymboltriangle(k);
+    	}
+    }
+
